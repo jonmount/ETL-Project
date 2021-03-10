ff51fa466fd2c1e53f94bedcc442059224738755
Jonathan Mount <jonmount@mdscom.com>
2021-03-09 23:04:37 -0700

    Final Edits

--- queries.sql
+++ queries.sql
@@ -4,17 +4,17 @@ DROP TABLE social_economic_score;
 
 -- Create tables for raw data to be loaded into
 CREATE TABLE world_bank_information (
-year INTEGER,
-Country_Name TEXT,
-Population INTEGER,
-Life_Expectancy DECIMAL(5,2),
-Fertility_Rate DECIMAL(5,2),
-GDP_Billions MONEY,
-GDP_per_Capita MONEY
+year_wb INTEGER,
+country_Name TEXT,
+population BIGINT,
+life_expectancy DECIMAL(5,2),
+fertility_rate DECIMAL(5,2),
+gdp_billions MONEY,
+gdp_per_capita MONEY
 );
 
 CREATE TABLE social_economic_score (
-year INTEGER,
+year_ses INTEGER,
 country TEXT,
 ses DECIMAL(5,2)
 );
@@ -23,6 +23,8 @@ ses DECIMAL(5,2)
 SELECT *
 FROM world_bank_information
 LIMIT 100;
+
+
 
 SELECT *
 FROM social_economic_score
@@ -34,12 +36,12 @@ FROM world_bank_information
 WHERE Country_Name = 'United States';
 
 -- Join table for SES
-SELECT w.year, w.Country_Name, w.Population, w.Life_Expectancy, w.Fertility_Rate, w.GDP_Billions, w.GDP_per_Capita, s.ses
+SELECT w.year_wb, w.Country_Name, w.population, w.life_expectancy, w.fertility_rate, w.gdp_billions, w.gdp_per_capita, s.ses
 INTO world_bank_final
 FROM world_bank_information AS w
 JOIN social_economic_score AS s
-ON s.year = w.year 
-AND s.country = w.Country_Name
+ON w.year_wb = s.year_ses
+AND s.country = w.country_name
 
 SELECT *
 FROM world_bank_final
